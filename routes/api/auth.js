f313ebdd1699f33f56759b65b0916735f9157243
mihon.testosterone <mihon.testosterone@gmail.com>
2021-11-26 10:23:17 +0200

    current route

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -9,5 +9,6 @@ const router = express.Router()
 router.post('/signup', validation(joiSchema), controllerWrapper(ctrl.signup))
 router.post('/login', validation(joiSchema), controllerWrapper(ctrl.login))
 router.get('/logout', authenticate, controllerWrapper(ctrl.logout))
+router.get('/current', authenticate, controllerWrapper(ctrl.current))
 
 module.exports = router
