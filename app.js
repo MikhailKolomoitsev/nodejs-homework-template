cbd9a67d6ec267f4ff364dc6bdf1440332f68031
mihon.testosterone <mihon.testosterone@gmail.com>
2021-11-28 18:16:52 +0200

    +

--- app.js
+++ app.js
@@ -1,29 +1,32 @@
 const express = require('express')
 const logger = require('morgan')
 const cors = require('cors')
-require('dotenv').config()
+const multer=require('multer')
 
-const contactsRouter = require('./routes/api/contacts')
-const authRouter = require('./routes/api/auth')
 
-const app = express()
+// require('dotenv').config()
 
-const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
+// const contactsRouter = require('./routes/api/contacts')
+// const authRouter = require('./routes/api/auth')
 
-app.use(logger(formatsLogger))
-app.use(cors())
-app.use(express.json())
+// const app = express()
 
-app.use('/api/users', authRouter)
-app.use('/api/contacts', contactsRouter)
+// const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 
-app.use((req, res) => {
-  res.status(404).json({ message: 'Not found' })
-})
+// app.use(logger(formatsLogger))
+// app.use(cors())
+// app.use(express.json())
 
-app.use((err, req, res, next) => {
-  const { status = 500, message = 'Server error' } = err
-  res.status(status).json({ message })
-})
+// app.use('/api/users', authRouter)
+// app.use('/api/contacts', contactsRouter)
 
-module.exports = app
+// app.use((req, res) => {
+//   res.status(404).json({ message: 'Not found' })
+// })
+
+// app.use((err, req, res, next) => {
+//   const { status = 500, message = 'Server error' } = err
+//   res.status(status).json({ message })
+// })
+
+// module.exports = app
